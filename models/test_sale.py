7a860d2a77a64bdb0c950f5c863e0fa0ca0d9bd3
trimanul <ivanko150702@gmail.com>
2022-11-12 23:22:41 +0300

    Добавил заполнение и проверку на длину поля

--- models/test_sale.py
+++ models/test_sale.py
@@ -1,11 +1,23 @@
 from random import randint
+from lxml import etree
+import json
 from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class TestSale(models.Model):
     _inherit = "sale.order"
 
-    test = fields.Char(string="Test", compute="_compute_random")
-    @api.depends("test")
-    def _compute_random(self):
-            for record in self:
-                record.test = randint(0, 100)
+    test = fields.Char(string="Test", default=str(randint(100, 10000)))
+    
+
+    @api.onchange("tax_totals_json")
+    def _onchange_line(self):
+        total = json.loads(self.tax_totals_json)
+        if (not (total['amount_total'] == 0)):
+            self.test = f"{total['amount_total']} - {self.date_order}"
+    
+    @api.constrains("test")
+    def check_test(self):
+        if (len(self.test) > 50):
+            raise ValidationError("Длина текста должна быть меньше 50 символов!")
+    
